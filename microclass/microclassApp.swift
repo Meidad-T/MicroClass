dc045d35f041021f5942b383bc909f30503abc1b
Meidad Troper <oro27@txstate.edu>
2025-10-24 20:27:03 -0500

    Initial Commit

---
+++ microclass/microclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  microclassApp.swift
+//  microclass
+//
+//  Created by Meidad Troper on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct microclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
